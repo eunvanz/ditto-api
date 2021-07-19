34a9fb01920adf94ba96ce43479c986ade1cebc9
Benjamin <eunvanz@naver.com>
2021-07-19 21:12:25 +0900

    fix: auth middleware 수정

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,11 +3,13 @@ import { NextFunction } from 'express';
 import * as admin from 'firebase-admin';
 
 export class AuthMiddleware implements NestMiddleware {
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(req: Request, _res: Response, next: NextFunction) {
     const token = req.headers.get('x-authorization');
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    const { uid } = decodedToken;
-    req.headers.set('uid', uid);
+    if (token) {
+      const decodedToken = await admin.auth().verifyIdToken(token);
+      const { uid } = decodedToken;
+      req.headers.set('uid', uid);
+    }
     next();
   }
 }
